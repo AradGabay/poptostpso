acf44f44ace2e49726d6df2b3de96f8d805a6947
Arad Gabay <ag7393@bs.amalnet.k12.il>
2019-11-08 11:41:06 +0200

    final commit

--- app/src/main/java/com/example/poptostpso/coloradded.java
+++ app/src/main/java/com/example/poptostpso/coloradded.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.LinearLayout;
 
 public class coloradded extends AppCompatActivity {
@@ -37,5 +38,9 @@ public class coloradded extends AppCompatActivity {
         else LR2.setBackgroundColor(Color.BLACK);
 
         return true;
+    }
+
+    public void prevact(View view) {
+        finish();
     }
 }
